1ac8286d2a6af463ca9b0f320e20adc9c62f6aa5
Valeria Aparecida <valeria777valeria@hotmail.com>
2025-11-17 10:24:09 -0300

    Configurando banco

--- mf-dev-backend-2025/Program.cs
+++ mf-dev-backend-2025/Program.cs
@@ -1,8 +1,13 @@
+using mf_dev_backend_2025.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 
